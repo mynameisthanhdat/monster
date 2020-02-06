638dc0b0cbde5082e8c98c020730b71ae472b4ed
ThanhDat <mynameisthanhdat@gmail.com>
2020-02-06 16:34:52 +0700

    Updates

---
+++ precache-manifest.4100c4d651f726c1fc79fd528762aad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527648a6ca89114ec744e85cab571475",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "208f4ab00d064d592f03",
+    "url": "/monster/static/css/main.f27bff59.chunk.css"
+  },
+  {
+    "revision": "b5893268c36500986bed",
+    "url": "/monster/static/js/2.3b93f435.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster/static/js/2.3b93f435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208f4ab00d064d592f03",
+    "url": "/monster/static/js/main.dc38cacd.chunk.js"
+  },
+  {
+    "revision": "395a62c0f61e762b1eeb",
+    "url": "/monster/static/js/runtime-main.480773ed.js"
+  }
+]);
